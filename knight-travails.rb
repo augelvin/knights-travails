3c5284f32cfd48bc3454d195827146e39e379796
Augelvin Theone Dermaga <augelvintd@gmail.com>
2023-03-29 22:18:10 -0700

    remove Knight and Board classes, add KnightTavails class

--- knight-travails.rb
+++ knight-travails.rb
@@ -1,11 +1,13 @@
-class Board
-  def initialize
-      @board = Array.new(8) {|i| Array.new(8) {|j| [i,j]}}
-  end
-end
-
-class Knight
-  def initialize
-    @moves = [[2, 1], [1, 2], [-1, 2], [-2, 1], [-2, -1], [-1, -2], [1, -2], [2, -1]]
-  end
+class KnightTravails
+    BOARD = Array.new(8) {|i| Array.new(8) {|j| [i,j]}}
+    MOVES = [[2, 1], [1, 2], [-1, 2], [-2, 1], [-2, -1], [-1, -2], [1, -2], [2, -1]]
+    
+    def initialize
+        @root
+    end
+    
+    def knight_moves(origin, destination)
+        # build tree from origin
+        # find shortrst path to destination
+    end
 end
