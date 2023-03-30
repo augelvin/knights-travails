ca7d3fe24c8cbb80f28404b8186f432e854d6b44
Augelvin Theone Dermaga <augelvintd@gmail.com>
2023-03-29 18:51:49 -0700

    add Knight class

--- knight-travails.rb
+++ knight-travails.rb
@@ -3,3 +3,9 @@ class Board
       @board = Array.new(8) {|i| Array.new(8) {|j| [i,j]}}
   end
 end
+
+class Knight
+  def initialize
+    @moves = [[2, 1], [1, 2], [-1, 2], [-2, 1], [-2, -1], [-1, -2], [1, -2], [2, -1]]
+  end
+end
