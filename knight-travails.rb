91eb336716cc715e1888eb49db4b5ee387a08d29
Augelvin Theone Dermaga <augelvintd@gmail.com>
2023-09-10 20:03:57 -0700

    remove empty spaces

--- knight-travails.rb
+++ knight-travails.rb
@@ -1,9 +1,16 @@
+class Node
+    def initialize(pos)
+        @pos = pos
+        # every possible moves
+    end
+end
+
 class KnightTravails
     BOARD = Array.new(8) {|i| Array.new(8) {|j| [i,j]}}
     MOVES = [[2, 1], [1, 2], [-1, 2], [-2, 1], [-2, -1], [-1, -2], [1, -2], [2, -1]]
     
     def initialize
-        @root
+        @root # make tree
     end
     
     def knight_moves(origin, destination)
